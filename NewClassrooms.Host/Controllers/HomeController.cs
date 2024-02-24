7fc9393560454b54b24d017e18b85b7fdd6ddcef
Shawn W Knight <shawn.knight@spatialfront.com>
2024-02-24 11:14:41 -0500

    Add Home Controller.

---
+++ NewClassrooms.Host/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="Knight Technologies LLC">
+// Author: Shawn W Knight
+// Copyright (c) Knight Technologies LLC All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace NewClassrooms.Host.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Provides an implementation of <see cref="Controller"/>.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Provides a method to return a default view.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ NewClassrooms.Host/Views/Home/Index.cshtml.cs
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------
+// <copyright file="Index.cshtml.cs" company="Spatial Front, Inc.">
+// Author: Shawn W Knight
+// Copyright (c) Spatial Front, Inc. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace CPDDMS.API.Host.Controllers.Views.Home
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    /// <summary>
+    /// Provides an implementation of <see cref="PageModel"/>.
+    /// </summary>
+    public class IndexModel : PageModel
+    {
+        /// <summary>
+        /// Provides a method for OnGet.
+        /// </summary>
+        public void OnGet()
+        {
+        }
+    }
+}
